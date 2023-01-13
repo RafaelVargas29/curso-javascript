d8cae1c0edcd8a35a2c8be41c9f9f502182215d8
Rafael Vargas <rafaelrgsv@gmail.com>
2023-01-13 16:42:57 -0300

    Module.exports sem colisão de nomes

--- commonjs/index.js
+++ commonjs/index.js
@@ -5,13 +5,19 @@ const fs = require("fs");
 const emoji = require("node-emoji");
 
 //importando um módulo que foi criado
-const fn = require("./modules/mod1");
-console.log("mod1");
-console.log(mod1.fn);
-console.log(mod1.fn());
+const mod1 = require("./modules/mod1");
+const mod2 = require("./modules/mod2");
+const mod3 = require("./modules/mod3");
+
+// console.log("mod1");
+// console.log(mod1.fn);
+// console.log(mod1.fn());
+
+console.log(mod2);
+console.log(mod3);
 
 //primeiro parâmetro é o nome do arquivo, segundo é o que estará escrito nele e o terceiro é uma função que retona um erro
-fs.writeFile("teste.txt", "Olá mundo!", err => {
-    if(err) throw err;
-    console.log("saved", emoji.get("coffee"));
-});
+// fs.writeFile("teste.txt", "Olá mundo!", err => {
+//     if(err) throw err;
+//     console.log("saved", emoji.get("coffee"));
+// });
