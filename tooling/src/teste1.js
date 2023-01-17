5481e6df13adf5f17d3396661ef17870a988e20b
Rafael Vargas <rafaelrgsv@gmail.com>
2023-01-17 17:15:27 -0300

    Configurar webpack

--- tooling/src/teste1.js
+++ tooling/src/teste1.js
@@ -1,27 +1,27 @@
-const teste = "teste 1";
+import axios from "axios"
 
-const arrowFn = n => n * n;
-console.log(arrowFn(2));
+const teste = "teste 9"
+const arrowFn = n => n * n
+console.log(arrowFn(2))
 
 class Teste {
     constructor(n) {
-        this.n = n;
+        this.n = n
     }
 }
-console.log(new Teste(5));
 
-//Função via cep
-const getAdress =  async (cep) => {
-    let url = `https://viacep.com.br/ws${cep}/json`;
+console.log(new Teste(5))
+
+const getAdress = async (cep) => {
+    let url = `https://viacep.com.br/ws/${cep}/json/`
     try {
-        const resposta = await fetch(url);
-        if(!resposta.ok) throw Error("Ivalid postal code!");
-        const json = await resposta.json();
-        return json;
+        const resposta = await axios.get(url)
+
+        const json = resposta.data
+        return json
     } catch (e) {
-        throw e;
+        throw e
     }
 }
-
-const endereco = getAdress("26.311-210");
-console.log(endereco);
+console.log("------")
+getAdress("03136-050").then(data => console.log(data))
